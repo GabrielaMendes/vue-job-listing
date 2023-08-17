72c11ce0d548ad97fd2f1113b75ca91a84d220c2
Gabriela <gdemelo.mendes@gmail.com>
2023-08-17 17:48:30 -0300

    config: added custom colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "desaturated-dark-cyan": " 	#5BA4A4",
+        "light-grayish-cyan-bg": "#EFFAFA",
+        "light-grayish-cyan-filter": "#EEF6F6",
+        "dark-grayish-cyan": "#7B8E8E",
+        "very-dark-grayish-cyan": "#2C3A3A",
+      },
+      fontFamily: {
+        "league-spartan": ["'League-Spartan', sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
 
