f6a0c8276d046a7d46ae1755c3c25520a519b818
Gabriela <gdemelo.mendes@gmail.com>
2023-08-19 17:55:33 -0300

    feature: created FilterItem component

---
+++ src/components/FilterItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  item: {
+    required: true,
+    type: String,
+  },
+  removable: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="cursor-pointer p-2 rounded-md font-bold  bg-light-grayish-cyan-filter text-desaturated-dark-cyan hover:bg-desaturated-dark-cyan hover:text-white hover:transition-hover"
+  >
+    {{ item }}
+  </div>
+</template>
